a21e9fecb17962afd56a0607f3f46be14a6ba203
gh-nomad <31376198+gh-nomad@users.noreply.github.com>
2025-01-10 01:18:23 -0300

    move structs RecvTable/RecvProp/ClientClass from SrcSDK.h to SourceEngineDumper.h

--- SourceEngineDumper.h
+++ SourceEngineDumper.h
@@ -2,9 +2,51 @@
 #include <Windows.h>
 #include <filesystem>
 #include "GHDumper.h"
-#include "SrcSDK.h"
 #include "Netvar.h"
 #include "SigData.h"
+
+// unnecessary class pointers have been converted to void* for simplicity
+
+struct RecvProp;
+
+struct RecvTable
+{
+	RecvProp*	m_pProps;
+	int			m_nProps;
+	void*		m_pDecoder;
+	char*		m_pNetTableName;
+	bool		m_bInitialized;
+	bool		m_bInMainList;
+};
+
+struct RecvProp
+{
+	char*			m_pVarName;
+	void*			m_RecvType;
+	int         	m_Flags;
+	int         	m_StringBufferSize;
+	int         	m_bInsideArray;
+	const void*		m_pExtraData;
+	RecvProp*		m_pArrayProp;
+	void*			m_ArrayLengthProxy;
+	void*			m_ProxyFn;
+	void*			m_DataTableProxyFn;
+	RecvTable*  	m_pDataTable;
+	int         	m_Offset;
+	int         	m_ElementStride;
+	int         	m_nElements;
+	const char*		m_pParentArrayPropName;
+};
+
+struct ClientClass
+{
+	void*			m_pCreateFn;
+	void*			m_pCreateEventFn;
+	char*			m_pNetworkName;
+	RecvTable*		m_pRecvTable;
+	ClientClass*	m_pNext;
+	int				m_ClassID;
+};
 
 class SourceEngineDumper : public Dumper
 {
